15f9087e28b75da932389d2b1088016d783d1d70
hgirish <girish@post.com>
2022-08-27 00:40:55 -0700

    GraphQL client

--- MovieApp/Client/Program.cs
+++ MovieApp/Client/Program.cs
@@ -8,4 +8,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+string graphQLServerPath = builder.HostEnvironment.BaseAddress + "graphql";
+
+builder.Services.AddMovieClient()
+   .ConfigureHttpClient(client =>
+   {
+       client.BaseAddress = new Uri(graphQLServerPath);
+   }
+);
+
 await builder.Build().RunAsync();
